c58a50f553bc10f082a3978649ec545b2b9eb0ad
Emmanuel <emmanuelrigobert000@gmail.com>
2025-03-04 19:54:15 +0100

    Zipkin and Docker Implementation

--- currency-exchange-service/src/main/java/com/emmanuel/microservices/currency_exchange_service/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/emmanuel/microservices/currency_exchange_service/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.emmanuel.microservices.currency_exchange_service;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class); //This is used to log messages
+
     @Autowired//This annotation is used to inject the CurrencyExchangeRespository bean into this class
     private CurrencyExchangeRespository repository;
 
@@ -20,6 +24,9 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);//This logs the message "retrieveExchangeValue called with {from} to {to}" values. To get these values, micrometer assigns a unique ID to each request and logs the request details.
+
 //        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(65));
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);//This method is used to get the exchange value from the database based on the from and to values
         if(currencyExchange == null){ //This condition checks if the entered from and to values are not found in the database
